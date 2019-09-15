50612dd9a1fb64985b658b50bfa5ce74b6dfa7a7
centerRyo <abc.atrn@gmail.com>
2019-09-15 17:45:18 +0900

    ヘッダー部分のスタイルを追記した

--- index.php
+++ index.php
@@ -2,11 +2,32 @@
 <html lang="ja">
   <head>
     <meta charset="utf-8">
-    <title>BizBook</title>
+    <title>BizBook | トップ</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP&display=swap" rel="stylesheet">
   </head>
   <body>
-    
+    <header>
+        <div class="site-width">
+            <h1><a href="index.php">BizBook</a></h1>
+            <nav id="top-nav">
+                <ul>
+                    <li><a href="signup.php">ユーザー登録</a></li>
+                    <li><a href="login.php">ログイン</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <!-- メインコンテンツ -->
+    <div id="contents" class="site-width">
+
+      <!-- サイドバー -->
+      <section id="sidebar">
+        <form class="" action="index.html" method="post">
+
+        </form>
+      </section>
+    </div>
   </body>
 </html>
---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>BizBook | ログイン</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <h1><a href="index.php">BizBook</a></h1>
+      <ul>
+        <li><a href="login.php">ログイン</a></li>
+        <li><a href="signup.php">ユーザー登録</a></li>
+      </ul>
+    </header>
+
+    <section>
+      <div class="for">
+
+      </div>
+    </section>
+
+    <footer>&copy; BizBook.com All Right Reserved.</footer>
+  </body>
+</html>
